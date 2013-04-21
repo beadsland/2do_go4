e45d829b33821dd5799e2c104557d4a3a3d323fa
beadsland <beads.d.land@gmail.com>
2013-04-21 07:35:20 -0400

    get file from ARG of from stdin

--- src/dogo.erl
+++ src/dogo.erl
@@ -26,7 +26,7 @@
 %% @author Beads D. Land-Trujillo [http://twitter.com/beadsland]
 %% @copyright 2013 Beads D. Land-Trujillo
 
-%% @version 0.0.4
+%% @version 0.0.5
 
 -define(module, dogo).
 
@@ -40,7 +40,7 @@
 -endif.
 % END POSE PACKAGE PATTERN
 
--version("0.0.4").
+-version("0.0.5").
 
 %%
 %% Include files
@@ -55,6 +55,7 @@
 -import(re).
 -import(filename).
 -import(string).
+-import(file).
 
 %%
 %% Exported Functions
@@ -69,7 +70,7 @@
 -export([do_run/2]).
 
 % private exports
--export([loop/2]).
+-export([loop/3, file_loop/3, run_file/4]).
 
 %%
 %% API Functions
@@ -93,55 +94,116 @@ run(IO, ARG, ENV) -> gen_command:run(IO, ARG, ENV, ?MODULE).
 
 %% @private Callback entry point for gen_command behaviour.
 do_run(IO, ARG) ->
-  ?STDOUT("Starting Dogo ~s list interpreter ~p~n",
-          [?VERSION(?MODULE), self()]),
-  do_file(IO, ARG).
+  Format = "Starting Dogo ~s list interpreter ~p~n",
+  ?STDOUT(Format, [?VERSION(?MODULE), self()]),
+  case ARG#arg.v of
+    []			-> do_captln(IO, ARG, stdin);
+    [File | _V]	-> Abs = filename:absname(File),
+                   do_file_read(IO, ARG, Abs)
+  end.
 
 %%
 %% Local Functions
 %%
 
-do_file(IO, ARG) ->
-  C = ARG#arg.cmd, [RelFile | V] = ARG#arg.v,
-  ?DEBUG("File: ~s~n", [RelFile]),
-  AbsFile = filename:absname(RelFile),
-  Eq = string:equal(RelFile, AbsFile),
-  if Eq		-> do_captln(IO, ARG);
-	 true	-> do_file(IO, ?ARG(C, [AbsFile | V]))
-  end.
-
 %%@private Export to allow for hotswap.
-loop(IO, ARG) ->
-  Stdin = IO#std.in,  
+loop(IO, ARG, Src) ->
   receive
-    {purging, _Pid, _Mod}					-> % chase your tail
+    {purging, _Pid, _Mod}							-> % chase your tail
       ?MODULE:loop(IO, ARG);
-    {'EXIT', Stdin, Reason}					->
-      ?DEBUG("~s: term: ~p~n", [ARG#arg.cmd, Reason]), exit(ok);
-    {'EXIT', _Pid, _Reason}					->
-      ?MODULE:loop(IO, ARG);
-    {stdout, Stdin, ".\n"} when IO#std.stop	->
-      ?DEBUG("~s: stop~n", [ARG#arg.cmd]), exit(ok);
-    {stdout, Stdin, eof}					->
-      ?DEBUG("~s: eof~n", [ARG#arg.cmd]), exit(ok);
-    {stdout, Stdin, Line}					->
-      do_procln(IO, ARG, Line);
-    Noise									->
-      ?STDERR("~s: noise: ~p~n", [ARG#arg.cmd, Noise]),
-      do_captln(IO, ARG)
+    {'EXIT', ExitPid, Reason}						->
+      do_exit(IO, ARG, Src, ExitPid, Reason);
+    {stdout, Stdin, Line} when Stdin == IO#std.in	->
+      do_line(IO, ARG, Src, Line);
+	{stderr, Stdin, Data} when Stdin == IO#std.in	->
+	  ?STDERR(Data);
+    Noise											->
+      do_noise(IO, ARG, Src, Noise)
   end.
 
-do_captln(IO, ARG) ->
-  Stdin = IO#std.in,
-  Stdin ! {stdin, self(), captln},
-  ?MODULE:loop(IO, ARG).
+do_file_read(IO, ARG, Src) ->
+  RunPid = spawn_link(?MODULE, run_file, [?IO(self()), ARG, ?ENV, Src]),
+  NewIO = ?IO(RunPid, IO#std.out, IO#std.err, false, false),
+  do_captln(NewIO, ARG, Src).
 
-do_procln(IO, ARG, [$\n]) -> do_captln(IO, ARG);
-do_procln(IO, ARG, [Space | Rest]) when Space == 32; Space == $\t ->
+run_file(IO, ARG, _ENV, Src) ->
+  case file:open(Src, [read]) of
+    {error, Reason} -> exit({Src, Reason});
+    {ok, Device}	-> ?MODULE:file_loop(IO, ARG, Device)
+  end.
+
+file_loop(IO, ARG, Device) ->
+  receive
+    {purging, _Pid, _Mod}								-> % chase your tail
+      ?MODULE:file_loop(IO, ARG, Device);
+    {'EXIT', ExitPid, Reason}							->
+      file_loop_exit(IO, ARG, Device, ExitPid, Reason);
+    {stdin, Stdout, captln} when Stdout == IO#std.out	->
+      file_read_line(IO, ARG, Device);
+    Noise												->
+      file_noise(IO, ARG, Device, Noise)
+  end.
+
+file_loop_exit(IO, ARG, Device, ExitPid, Reason) ->
+  case ExitPid of
+    Stdin when Stdin == IO#std.in	->
+      ?DEBUG("~s: file_loop: ~p~n", [ARG#arg.cmd, Reason]), exit(ok);
+    _ 								->
+      ?MODULE:file_loop(IO, ARG, Device)
+  end.
+
+file_read_line(IO, ARG, Device) ->
+  case io:get_line(Device, "") of
+    eof		-> file:close(Device), exit(ok);
+    Line	-> ?STDOUT(Line), file_loop(IO, ARG, Device)
+  end.
+
+file_noise(IO, ARG, Device, Noise) ->
+  ?STDERR("~s: noise: ~p~n", [ARG#arg.cmd, Noise]),
+  file_loop(IO, ARG, Device).
+
+% Handle a line of input.
+do_line(IO, ARG, Src, Line) ->
+  case Line of
+    ".\n" when IO#std.stop	->
+      exit(ok);
+    eof						->
+      exit(ok);
+    _						->
+      do_procln(IO, ARG, Src, Line)
+  end.
+
+% Process a line of dogo format input.
+do_procln(IO, ARG, Src, [$\n]) -> do_captln(IO, ARG, Src);
+do_procln(IO, ARG, Src, [Space | Rest]) when Space == 32; Space == $\t ->
   {ok, MP} = re:compile("^[\\t ]*\\n$"),
   case re:run(Rest, MP, [{capture, none}]) of
-	match	-> do_captln(IO, ARG);
-	nomatch -> ?STDOUT(IO), do_captln(IO, ARG)
+    match	-> do_captln(IO, ARG, Src); 			% ignore blank line
+    nomatch -> ?STDOUT(IO), do_captln(IO, ARG, Src)
   end;
-do_procln(IO, ARG, [$# | _Rest]) -> do_captln(IO, ARG);
-do_procln(IO, ARG, Line) -> ?STDOUT(Line), do_captln(IO, ARG).
+do_procln(IO, ARG, Src, [$# | _Rest]) -> do_captln(IO, ARG, Src);
+do_procln(IO, ARG, Src, Line) -> ?STDOUT(Line), do_captln(IO, ARG, Src).
+
+% Send a message to capture another line from stdin.
+do_captln(IO, ARG, Src) ->
+  Stdin = IO#std.in,
+  Stdin ! {stdin, self(), captln},
+  ?MODULE:loop(IO, ARG, Src).
+
+% Handle process exit messages.
+do_exit(IO, ARG, Src, ExitPid, Reason) ->
+  case ExitPid of
+    Stdin when Stdin == IO#std.in	->
+	  case Reason of
+		ok			-> exit(ok);
+		{ok, What}	-> exit({ok, What});
+		_Else		-> exit({ARG#arg.cmd, Reason})
+	  end;
+    _ 								->
+      ?MODULE:loop(IO, ARG, Src)
+  end.
+
+% Handle noise on the message queue.
+do_noise(IO, ARG, Src, Noise) ->
+  ?STDERR("~s: noise: ~p~n", [ARG#arg.cmd, Noise]),
+  do_captln(IO, ARG, Src).
